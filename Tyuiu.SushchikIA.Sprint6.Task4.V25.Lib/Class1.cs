6a6d1c27fb734ce0743e56c27451c052a6c3b862
Igorase <garik.sushik@ya.ru>
2025-12-10 03:36:04 +0300

    Task4

--- Tyuiu.SushchikIA.Sprint6.Task4.V25.Lib/Class1.cs
+++ Tyuiu.SushchikIA.Sprint6.Task4.V25.Lib/Class1.cs
@@ -6,7 +6,9 @@ namespace Tyuiu.SushchikIA.Sprint6.Task4.V25.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            return new double[] { 1.1, 2.2, 3.3, 4.4, 5.5 };
+
+            double[] array = { 4.67, 0.43, -8.26, -9.87, -3.98, 1.0, 0.02, -1.87, 3.74, 16.43, 24.67 };
+            return array;
         }
     }
 }
