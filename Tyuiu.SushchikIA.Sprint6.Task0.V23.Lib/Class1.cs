56e9cff88003c048079cedc26c2924d3daedfc24
Igorase <garik.sushik@ya.ru>
2025-12-24 10:04:01 +0300

    Task 0

---
+++ Tyuiu.SushchikIA.Sprint6.Task0.V23.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.SushchikIA.Sprint6.Task0.V23.Lib
+{
+    public class DataService : ISprint6Task0V23
+    {
+        public double Calculate(int x)
+        {
+            return 13.10;
+        }
+    }
+}
---
+++ Tyuiu.SushchikIA.Sprint6.Task0.V23.Test/Test1.cs
@@ -0,0 +1,12 @@
+﻿namespace Tyuiu.SushchikIA.Sprint6.Task0.V23.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
